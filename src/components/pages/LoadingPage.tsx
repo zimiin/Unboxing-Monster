f8b0938145ab244b1d3f708e06c9094d60196315
Jimin Shin <kahin6105@gmail.com>
2021-08-17 10:30:56 +0900

    Add loading image

--- src/components/pages/LoadingPage.tsx
+++ src/components/pages/LoadingPage.tsx
@@ -8,10 +8,10 @@ import {
 
 const LoadingPage = ({route, navigation}: LoadingProps) => {
   console.log('box id: ' + route.params.boxId + ' count: ' + route.params.count)
-  
+  // 오픈 요청 보내고 대기
+
   return (
-    <LoadingTemplate
-    
+    <LoadingTemplate  
     />
   )
 }
--- src/components/templates/LoadingTemplate.tsx
+++ src/components/templates/LoadingTemplate.tsx
@@ -1,13 +1,39 @@
+import Bold from '@components/atoms/typography/Bold'
+import { IMAGES } from '@constants/images'
 import React from 'react'
 import {
   View,
   Text,
+  Image,
 } from 'react-native'
+import { scale } from 'react-native-size-matters'
 
 const LoadingTemplate = () => {
   return (
-    <View>
-      <Text>This is ComponentName.</Text>
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        flex: 1,
+      }}
+    >
+      <Bold
+        style={{
+          fontSize: 17,
+          marginBottom: 30,
+        }}
+      >
+        블록체인에 연결 중...
+      </Bold>
+
+      <Image
+        source={IMAGES.open_loading}
+        style={{
+          width: scale(278),
+          height: scale(256)
+        }}
+      />
     </View>
   )
 }
