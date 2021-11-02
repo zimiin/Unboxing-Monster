c89bd080cec0c17e387614ddf00f3580c054ed9b
zimiin <kahin6105@gmail.com>
2021-11-02 10:11:35 +0900

    Implement PollInit screen

--- src/components/templates/PollInitTemplate.tsx
+++ src/components/templates/PollInitTemplate.tsx
@@ -1,5 +1,9 @@
+import HalfWidthButton from '@components/atoms/button/HalfWidthButton'
+import { COLORS } from '@constants/colors'
+import { scale, verticalScale } from '@constants/figure'
+import { IMAGES } from '@constants/images'
 import React from 'react'
-import { SafeAreaView, Text, TouchableOpacity } from 'react-native'
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 interface Props {
   onPressStartPoll: () => void,
@@ -8,20 +12,96 @@ interface Props {
 
 function PollInitTemplate(props: Props) {
   return (
-    <SafeAreaView>
-      <TouchableOpacity
-        onPress={props.onPressStartPoll}
-      >
-        <Text>Start Poll!!</Text>
-      </TouchableOpacity>
+    <SafeAreaView
+      style={{
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: 'white',
+      }}
+    >
+      <Image
+        source={IMAGES.poll_event}
+        style={{
+          width: scale(264),
+          height: scale(200),
+          resizeMode: 'contain',
+          marginTop: verticalScale(100)
+        }}
+      />
 
-      <TouchableOpacity
-        onPress={props.onPressGoToStorage}
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+        }}
       >
-        <Text>Just go to storage.</Text>
-      </TouchableOpacity>
+        <Text
+          style={styles.description}
+        >
+          설문조사에 참여하시면
+        </Text>
+
+        <Text
+          style={styles.description}
+        >
+         <Text
+          style={styles.descriptionBold}
+          > 
+            3,000포인트
+          </Text>
+          를 더 드려요!
+        </Text>
+
+        <Text
+          style={[
+            styles.description,
+            {
+              marginBottom: verticalScale(20),
+            }
+          ]}
+        >
+          참여하시겠어요?
+        </Text>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            marginBottom: verticalScale(20),
+          }}
+        >
+          <HalfWidthButton
+            buttonStyle={{
+              backgroundColor: '#bdbdbd',
+              marginRight: scale(10),
+            }}
+            onPress={props.onPressGoToStorage}
+            text='안할래요'
+            textStyle={{
+              color: '#454545',
+            }}
+          />
+
+          <HalfWidthButton
+            onPress={props.onPressStartPoll}
+            text='네 참여할래요'
+          />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
 
 export default PollInitTemplate
+
+const styles = StyleSheet.create({
+  description: {
+    fontFamily: 'GmarketSansTTFLight',
+    textAlign: 'center',
+    fontSize: 17,
+  },
+  descriptionBold: {
+    fontFamily: 'GmarketSansTTFMedium',
+    textAlign: 'center',
+    fontSize: 17,
+  }
+})
